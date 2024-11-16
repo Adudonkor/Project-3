483ec347ca0de21326ca4ef64299fa6981a723a3
Me4myself22 <meganecm9@hotmail.com>
2024-11-15 19:37:02 -0500

    Create main.py
    Add main.py with placeholder code for project integration

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is the central file for all the  project components
+#The code below is just a sample/place holder
+
+from modules.caregiver import Caregiver
+from modules.schedule import Schedule
+from modules.payroll import Payroll
+
+def main():
+    # Placeholder for project components
+    print("Welcome to the Scheduling Program")
+
+if __name__ == "__main__":
+    main()
