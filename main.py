3504d822e3f1a87356c20ae897e4b40ee9644b0c
Me4myself22 <meganecm9@hotmail.com>
2024-11-22 20:56:44 -0500

    Update main.py
    added all instances

--- main.py
+++ main.py
@@ -6,7 +6,58 @@ from modules.payroll import Payroll
 
 def main():
     # Caregiver and availability insatnce
+    caregivers = [
+        Caregiver("Mark", "123-456-7890", "Mark@example.com", 20),
+        Caregiver("Ben", "634-326-2130", "Ben@example.com", 20),
+        Caregiver("Clara", "764-254-4567", "Clara@example.com"),
+        Caregiver("Tawi", "856-346-1351", "Tawi@example.com", 20),
+        Caregiver("Dumpi", "756-235-6131", "Dumpi@example.com"),
+        Caregiver("Shay", "555-777-6814", "Shay@example.com"),
+        Caregiver("Ron", "111-222-3333", "Ron@example.com"),
+        Caregiver("Wendy", "444-555-6666", "Wendy@example.com", 20),
+    ]
 
+# Setting up caregivers' availability
+    caregivers[0].update_availability("7:00 AM - 1:00 PM", "preferred")
+    caregivers[0].update_availability("1:00 PM - 7:00 PM", "available")
+
+    caregivers[1].update_availability("7:00 AM - 1:00 PM", "available")
+    caregivers[1].update_availability("1:00 PM - 7:00 PM", "unavailable")
+
+    caregivers[2].update_availability("7:00 AM - 1:00 PM", "unavailable")
+    caregivers[2].update_availability("1:00 PM - 7:00 PM", "preferred")
+
+    caregivers[3].update_availability("7:00 AM - 1:00 PM", "unavailable")
+    caregivers[3].update_availability("1:00 PM - 7:00 PM", "available")
+
+    caregivers[4].update_availability("7:00 AM - 1:00 PM", "available")
+    caregivers[4].update_availability("1:00 PM - 7:00 PM", "unavailable")
+
+    caregivers[5].update_availability("7:00 AM - 1:00 PM", "unavailable")
+    caregivers[5].update_availability("1:00 PM - 7:00 PM", "available")
+
+    caregivers[6].update_availability("7:00 AM - 1:00 PM", "preferred")
+    caregivers[6].update_availability("1:00 PM - 7:00 PM", "unavailable")
+
+    caregivers[7].update_availability("7:00 AM - 1:00 PM", "available")
+    caregivers[7].update_availability("1:00 PM - 7:00 PM", "available")
+
+# Add caregivers to the schedule
+    schedule = Schedule()
+    for caregiver in caregivers:
+        schedule.add_caregiver(caregiver)
+
+    # Add shifts to the availability schedule
+    schedule.shifts["2024-11-21"] = [Shift("7:00 AM - 1:00 PM"), Shift("1:00 PM - 7:00 PM")]
+    schedule.shifts["2024-11-22"] = [Shift("7:00 AM - 1:00 PM"), Shift("1:00 PM - 7:00 PM")]
+    schedule.shifts["2024-11-23"] = [Shift("7:00 AM - 1:00 PM"), Shift("1:00 PM - 7:00 PM")]
+    schedule.shifts["2024-11-24"] = [Shift("7:00 AM - 1:00 PM"), Shift("1:00 PM - 7:00 PM")]
+    schedule.shifts["2024-11-25"] = [Shift("7:00 AM - 1:00 PM"), Shift("1:00 PM - 7:00 PM")]
+
+
+    # Generate and view the schedule
+    schedule.assign_shifts()
+    schedule.view_schedule()
 
 
 
